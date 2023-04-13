e1ce556fb44c8c9c589db8a0b30a8eec043adb18
Silver <103918033+revlis-12@users.noreply.github.com>
2023-04-14 01:39:58 +0200

    Update submitssion_gen.sh

--- submitssion_gen.sh
+++ submitssion_gen.sh
@@ -4,6 +4,7 @@ head -2 nn.py > submission.py
 tail -n +3 myFuncTools.py >> submission.py
 tail -n +5 nn.py >> submission.py
 sed "s/nn.//g" test.py | tail -n +4 >> submission.py
+sed -i "s/autoHyperTung/autoHyperTunning" submission.py
 cd ..
 cp my-tensorflow/submission.py ./nn.py
 rm -rf my-tensorflow
